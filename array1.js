d8413d78bdb1b61d5569754b1b4aad8db179c7f7
hirwavestine <gilhirwa22@gmail.com>
2019-03-24 23:14:26 +0200

    updating array element

--- array1.js
+++ array1.js
@@ -1,7 +1,11 @@
 //declare fruits
 const fruits=["apple", "banana", "orange"];
+
 console.log(fruits);
 //get apple
 console.log(fruits[0]);
 //getting orange
+console.log(fruits[2]);
+//update element of array
+fruits[2]="grape";
 console.log(fruits[2]);
