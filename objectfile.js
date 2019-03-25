2d7ca38d620562100d663dcf9f17c54409dbdda6
hirwavestine <gilhirwa22@gmail.com>
2019-03-25 03:55:34 +0200

    handling underfined

--- objectfile.js
+++ objectfile.js
@@ -1,15 +1,18 @@
-//arrays&iteration
+
 const characters=[{name:"KEn the Ninja", age:30},
                 {name:"Master White", age:10000},
                 {name:"Ben the Baby Ninja",age:5}
             ];
-            //complete the for loop
+            
 for(i=0; i<characters.length;i++){
     console.log("........");
-    //define the character const
 const character=characters[i];
 //print "My name is ---""
     console.log(`My name is ${character.name}`);
-//print "I am --- years old"
+    //add if statements below
+    //Handling undefined
+    if(character.age===undefined){
+        console.log("My age is a secret!");
+         }else{
     console.log(`I am ${character.age} years old`);
 }
