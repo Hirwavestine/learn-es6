935e656647e6882ecbb71329c1bf3a47b0b5f88b
hirwavestine <gilhirwa22@gmail.com>
2019-03-25 02:37:21 +0200

    objects as arrays elements

--- objectfile.js
+++ objectfile.js
@@ -1,5 +1,3 @@
-const item={name:"shrunk", price: 30}
-//getting object values this line will print shrunk
-//updating the values of the price property
-item.price=50;
-console.log(item.price);
+//objects as array elements
+const item=[{name:"shrunk", price: 30},
+            {name:"Ninja Sword", price: 120}];
