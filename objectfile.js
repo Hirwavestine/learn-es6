b8cdd674d44ad845909f837582c0bfcbfdb6df94
hirwavestine <gilhirwa22@gmail.com>
2019-03-25 02:40:11 +0200

    get objects in  arrays

--- objectfile.js
+++ objectfile.js
@@ -1,3 +1,6 @@
 //objects as array elements
 const item=[{name:"shrunk", price: 30},
-            {name:"Ninja Sword", price: 120}];
+            {name:"Ninja Sword", price: 120}
+        ];
+ //get object in array
+ console.log(item[1]);
