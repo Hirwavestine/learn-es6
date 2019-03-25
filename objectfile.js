c09a9de2c0d0c336d88c62b1f54657ed2a09f7d0
hirwavestine <gilhirwa22@gmail.com>
2019-03-25 02:24:27 +0200

    the values of the price property

--- objectfile.js
+++ objectfile.js
@@ -1,3 +1,5 @@
 const item={name:"shrunk", price: 30}
 //getting object values this line will print shrunk
-console.log(item.name);
+//updating the values of the price property
+item.price=50;
+console.log(item.price);
