022e79674c54c31bd558caa05d1aa11596b123e6
hirwavestine <gilhirwa22@gmail.com>
2019-03-25 03:36:33 +0200

    array and iteration

--- objectfile.js
+++ objectfile.js
@@ -3,3 +3,13 @@ const characters=[{name:"KEn the Ninja", age:30},
                 {name:"Master White", age:10000},
                 {name:"Ben the Baby Ninja",age:5}
             ];
+            //complete the for loop
+for(i=0; i<characters.length;i++){
+    console.log("........");
+    //define the character const
+const character=characters[i];
+//print "My name is ---""
+    console.log(`My name is ${character.name}`);
+//print "I am --- years old"
+    console.log(`I am ${character.age} years old`);
+}
