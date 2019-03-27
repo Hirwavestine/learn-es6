b726908bb3ae5406e01800d9803e3c897336d162
hirwavestine <gilhirwa22@gmail.com>
2019-03-27 21:23:39 +0200

    Add a menus property and assign an array to it and Use a for loop to print the contents of the menu array

--- cafe.js
+++ cafe.js
@@ -4,6 +4,14 @@ const cafe={
         opening:"10:00(AM)",
         closing:"17:00(PM)"
     },
+     // Add a menus property and assign an array to it
+    menus:["coffee", "the", "chocolate cake"]
 };
     console.log(`Name: ${cafe.name}`);
     console.log(`From ${cafe.businessHours.opening} to $(cafe.businessHours.closing)`);
+    console.log(`----------`);
+    console.log('Menu Recommandations');
+    // Use a for loop to print the contents of the menu array
+    for(let i=0; i<menus.length; i++){
+        console.log(cafe.menus[i]);
+    }
