c6e8e0ef66548561f132e7cb52f619401d0f8a02
hirwavestine <gilhirwa22@gmail.com>
2019-03-27 20:33:36 +0200

    add cafe.js to display inf if cafe resto

---
+++ cafe.js
@@ -0,0 +1,9 @@
+const cafe={
+    name:"Progate cafe",
+    businessHours:{
+        opening:"10:00(AM)",
+        closing:"17:00(PM)"
+    },
+};
+    console.log(`Name: ${cafe.name}`);
+    console.log(`From ${cafe.businessHours.opening} to $(cafe.businessHours.closing)`);
