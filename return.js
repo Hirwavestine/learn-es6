d89ff6920918440f4d8baeeee9cfcca91aebeac5
hirwavestine <gilhirwa22@gmail.com>
2019-04-01 09:39:35 +0200

    return types

--- return.js
+++ return.js
@@ -1,7 +1,8 @@
-const add=(a,b)=>{
-    return a+b;
-}
-//call functon
-const sum=(2,9);
-console.log(sum);
-//output will be 11
+const check=(number)=>{
+    //tue or false(boolean)
+    return number % 2 === 0;
+};
+//output is true
+console.log(check(6));
+//output will be false
+console.log(check(5));
