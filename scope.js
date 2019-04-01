469b949e7b7ad777e4b9d32783f018ab9a28e404
hirwavestine <gilhirwa22@gmail.com>
2019-04-01 09:27:05 +0200

    define a constant inside a function

--- scope.js
+++ scope.js
@@ -1,6 +1,7 @@
-const name="Ken the Ninja";
+
 const introduce=()=>{
+    const name="Ken the Ninja";
     //constant name can be used within this scope
 
 }
-//const name be used within this scope
+//const name can not  be used within this scope
